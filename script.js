b5f6d1f39ec1d01c077fa384e4a0e62f838132af
Cata <george.fusa97@yahoo.com>
2023-03-02 22:01:08 +0200

    Version of the game which works in console Implemented game and playRound functions

--- script.js
+++ script.js
@@ -1,7 +1,64 @@
 // code here
 
-// function that implements a round
+// function that implements a 5 round game and keeps score
+
+function game(){
+    
+    //these two variables will keep the score
+    let computerScore = 0;
+    let playerScore = 0;
+
+    for (let i=0; i<2; i++)
+    {
+        //we prompt the user for input
+        let playerSelection = prompt("Please enter rock, paper or scissors: ");
+        //play one round and decide who wins, also increment the score of the winner.
+        playRound(playerSelection,getComputerChoice())? playerScore++ : computerScore++;
+    }
+
+    console.log(`The final score is: ${playerScore} to ${computerScore}`);
+    if(playerScore == computerScore)
+    {
+    console.log("Draw!!!");
+    return;
+    }
+    console.log((playerScore > computerScore)? "Player wins!" : "Computer wins!");
+}
+
+// function that implements a round\
+// if the function returns 0, then computer won. Else, if it returns 1, then player won.
 function playRound(playerSelection, computerSelection){
+
+    if (playerSelection.toLowerCase() == "rock" && computerSelection.toLocaleLowerCase() == "paper")
+    {
+        console.log(`Computer wins, ${computerSelection} beats ${playerSelection}`);
+        return 0;
+    }
+    else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLocaleLowerCase() == "scissors") 
+    {
+        console.log(`Player wins, ${playerSelection} beats ${computerSelection}`);
+        return 1;
+    }
+    else if (playerSelection.toLowerCase() == "paper" && computerSelection.toLocaleLowerCase() == "scissors")
+    {
+        console.log(`Computer wins, ${computerSelection} beats ${playerSelection}`);
+        return 0;
+    }
+    else if (playerSelection.toLowerCase() == "paper" && computerSelection.toLocaleLowerCase() == "rock")
+    {
+        console.log(`Player wins, ${playerSelection} beats ${computerSelection}`);    
+        return 1;
+    }
+    else if (playerSelection.toLowerCase() == "scissors" && computerSelection.toLocaleLowerCase() == "rock")
+    {
+        console.log(`Computer wins, ${computerSelection} beats ${playerSelection}`);
+        return 0;
+    }
+    else if (playerSelection.toLowerCase() == "scissors" && computerSelection.toLocaleLowerCase() == "paper")
+    {
+        console.log(`Player wins, ${playerSelection} beats ${computerSelection}`);
+        return 1;
+    }
 }
 
 
@@ -14,5 +71,4 @@ function getComputerChoice(){
     return a < 0.33? 'Paper':'Scissors'
 }
 
-const playerChoice = "rock";
-const computerChoice = getComputerChoice();
+game();
